2f5d4d6617169b4618e6c7d199785d28653ce10d
Mohammad Raza <raza.it7522@gmail.com>
2023-03-22 11:07:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Super-Hero-App
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Get the image and powerstats for the superhero when searched by name
+- Get the image and powerstats of a random superhero when the "Get new hero" button is clicked
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Links
+
+- Live Site URL: [Live site URL](https://raza7522.github.io/Super-Hero-App/)
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- API
+- Desktop-first workflow
---
+++ script.js
@@ -0,0 +1,70 @@
+
+// https://www.superheroapi.com/api.php/211022344610929/245
+
+const SUPERHERO_TOKEN = "211022344610929";
+const BASE_URL = `https://www.superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
+
+const newHeroButton = document.querySelector("#new-hero-btn");
+
+const heroImageDiv = document.querySelector("#heroImage");
+
+const searchButton = document.querySelector("#search-btn");
+
+const searchInput = document.querySelector("#search-input");
+
+
+// ! Function for getting random super hero
+const getSuperHero = (id) => {
+
+    fetch(`${BASE_URL}/${id}`)
+
+        .then(response => response.json())
+
+        .then(json => {
+            const superHero = json;
+            showHeroInfo(superHero);
+        })
+}
+
+// !Function for showing super hero info
+const showHeroInfo = (character) => {
+
+    const name = `<h2>${character.name}</h2>`;
+
+    const img = `<img src = "${character.image.url}" height=200 witdh=200 />`;
+
+    const stats = Object.keys(character.powerstats).map(stat => {
+        return `<p>${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`
+    }).join("");
+
+    heroImageDiv.innerHTML = `${name}${img}${stats}`;
+}
+
+
+// ! Function for getting super hero on searching
+const getSearchSuperHero = (name) => {
+
+    fetch(`${BASE_URL}/search/${name}`)
+        .then(response => response.json())
+        .then(json => {
+            const hero = json.results[0];
+
+            showHeroInfo(hero);
+        })
+}
+
+// ! Function for generating random number
+const randomHero = function () {
+    const numberOfHeroes = 731;
+    return Math.floor(Math.random() * numberOfHeroes) + 1;
+}
+
+newHeroButton.onclick = () => {
+    getSuperHero(randomHero());
+    heroImageDiv.classList.add("active");
+}
+
+searchButton.onclick = () => {
+    getSearchSuperHero(searchInput.value);
+    heroImageDiv.classList.add("active");
+}
